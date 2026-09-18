7212770e780464d2bd61a647eb17ec7ba5947f9b
Tariful Hasan <tarif305@gmail.com>
2026-09-18 09:22:41 +0600

    register on progress

--- src/Auth/Register.jsx
+++ src/Auth/Register.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div>
+      <form>
+        <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input type="email" className="input" placeholder="Email" />
+          <label className="label">Password</label>
+          <input type="password" className="input" placeholder="Password" />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Register</button>
+        </fieldset>
+      </form>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
