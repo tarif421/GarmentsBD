bdcc633524f579f88f044d606c4274ec22f2da8a
Tariful Hasan <tarif305@gmail.com>
2026-09-19 10:55:56 +0600

    useAuth

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import AuthContext from "./AuthContext";
 
 const AuthProvider = ({ children }) => {
+  // register user function
   const registerUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
+  // login user function
+  const loginUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
+  // authInfo object to provide the functions and state to the context
   const authInfo = {
     registerUser,
+    loginUser,
   };
 
   return <AuthContext value={authInfo}>{children}</AuthContext>;
